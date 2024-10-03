057846baf9460abeb3ee7db67fc93139537ec5f8
Luis Fernando Olmos Berrio <luisfernandoolmosberrio56@gmail.com>
2024-10-03 16:47:33 -0500

    Create InputComponet

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from './components/input/input.component';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
-
+const Components = [InputComponent];
+const Modules = [CommonModule, IonicModule, FormsModule];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [...Components],
+  imports: [...Modules],
+  exports: [...Components, ...Modules]
 })
-export class SharedModule { }
+export class SharedModule {}
