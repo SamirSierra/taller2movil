26634ef34458b370a73e01f30fef8b5027753e91
GabrielCarrilloF <gabocarrillo.deve@gmail.com>
2024-10-08 12:06:02 -0500

    Descripción de mis cambios

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,8 +6,9 @@ import { FormsModule } from '@angular/forms';
 import { AvatarComponent } from './components/avatar/avatar.component';
 import { ButtonComponent } from './components/button/button.component';
 import { AuthService } from './services/auth/auth.service';
+import { FootComponent } from './components/foot/foot.component';
 
-const Components = [InputComponent, AvatarComponent, ButtonComponent];
+const Components = [InputComponent, AvatarComponent, ButtonComponent, FootComponent];
 const Modules = [CommonModule, IonicModule, FormsModule];
 
 @NgModule({
