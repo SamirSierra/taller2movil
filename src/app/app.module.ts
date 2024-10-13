779caf27413ec1bc38aa9dc7679a686d9e6c79a3
jesusbrave <jvaliente737@gmial.com>
2024-10-13 17:40:40 -0500

    cartas con estilo, en tiempo real, actualizar, eliminar, perzonalizadas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BaseRouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule} from '@angular/fire/compat/auth'
-
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { environment } from 'src/environments/environment.prod';
 import { CoreModule } from './core/core/core.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CartareasComponent } from './cartareas/cartareas.component';
 
 @NgModule({
@@ -25,9 +23,10 @@ import { CartareasComponent } from './cartareas/cartareas.component';
     AngularFireAuthModule,
     ReactiveFormsModule,
     AngularFirestoreModule,
+    FormsModule
   ],
   providers: [
-    { provide: BaseRouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: BaseRouteReuseStrategy, useClass: IonicRouteStrategy }, // Coma agregada aquí
   ],
   bootstrap: [AppComponent],
 })
--- src/app/cartareas/cartareas.component.ts
+++ src/app/cartareas/cartareas.component.ts
@@ -1,14 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Tarea {
+  id: number;
+  titulo: string;
+  descripcion: string;
+  fecha: string;
+  color: string;  // Propiedad de color
+}
 
 @Component({
   selector: 'app-cartareas',
   templateUrl: './cartareas.component.html',
   styleUrls: ['./cartareas.component.scss'],
 })
-export class CartareasComponent  implements OnInit {
+export class CartareasComponent {
+  tareas: Tarea[] = [];
+  nuevoTitulo: string = '';
+  nuevaDescripcion: string = '';
+  nuevaFecha: string = '';
+  tareaEditando: Tarea | null = null;
+  colorSeleccionado: string = ''; // Color seleccionado por el usuario
 
-  constructor() { }
+  agregarTarea() {
+    const colorFinal = this.colorSeleccionado || 'dark'; // Usar blanco si no hay color seleccionado
 
-  ngOnInit() {}
+    if (this.tareaEditando) {
+      this.tareaEditando.titulo = this.nuevoTitulo;
+      this.tareaEditando.descripcion = this.nuevaDescripcion;
+      this.tareaEditando.fecha = this.nuevaFecha;
+      this.tareaEditando.color = colorFinal; // Actualizar color en edición
+      this.tareaEditando = null; // Limpiar después de actualizar
+    } else {
+      const nuevaTarea: Tarea = {
+        id: Date.now(),
+        titulo: this.nuevoTitulo,
+        descripcion: this.nuevaDescripcion,
+        fecha: this.nuevaFecha,
+        color: colorFinal,  // Asignar color seleccionado
+      };
+      this.tareas.push(nuevaTarea);
+    }
+    this.limpiarCampos();
+  }
 
+  eliminarTarea(id: number) {
+    this.tareas = this.tareas.filter(tarea => tarea.id !== id);
+  }
+
+  iniciarEdicion(tarea: Tarea) {
+    this.tareaEditando = tarea;
+    this.nuevoTitulo = tarea.titulo;
+    this.nuevaDescripcion = tarea.descripcion;
+    this.nuevaFecha = tarea.fecha;
+    this.colorSeleccionado = tarea.color; // Establecer color al editar
+  }
+
+  limpiarCampos() {
+    this.nuevoTitulo = '';
+    this.nuevaDescripcion = '';
+    this.nuevaFecha = '';
+    this.colorSeleccionado = ''; // Limpiar color seleccionado
+  }
 }
